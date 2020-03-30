f7d50d00c4ea89790c909e78c87bde5475d702d3
n-wbrown <n-wbrown@users.noreply.github.com>
2020-03-30 12:22:14 -0700

    DEV: unrecognized arguments are passed directly to pytest

--- pmps_test/bin/main.py
+++ pmps_test/bin/main.py
@@ -2,21 +2,41 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+import argparse
 import os
 from pathlib import PurePath
+import sys
+from textwrap import dedent, fill
 
 import pytest
 
 import pmps_test
 
-def main():
-    pytest.main([
+
+DESCRIPTION = fill(dedent("""
+    Test suite for PMPS. Unrecognized arguments will be passed directly to
+    pytest.
+    """)).strip()
+
+def main(args=None):
+
+    top_parser = argparse.ArgumentParser(
+        prog="pmps_test",
+        description=DESCRIPTION,
+    )
+    
+    args, unknown_args = top_parser.parse_known_args(args)
+
+    print(args)
+    print(unknown_args)
+
+    pytest_args = [
         "--pyargs",
         "pmps_test.pmps",
-        #"-vrxs",
-    ])
-    print("Main has run")
+    ] + unknown_args
 
+    pytest.main(pytest_args)
+    
 
 if __name__ == "__main__":
     main()
